3f2a12ff6543e117242939e8f49de9bd66cc32e0
matteo <matteo.jeulin@gmail.com>
2024-07-03 10:58:17 +0200

    Bumped the version adn updated the build system

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,15 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import del from 'rollup-plugin-delete'
 import { dts } from "rollup-plugin-dts";
+import { readFileSync } from 'fs';
 
 const PACKAGE_NAME = "three-js-blobtree"
+const PACKAGE_JSON = JSON.parse(readFileSync('package.json', 'utf8'))
+
+const external = [
+    ...Object.keys(PACKAGE_JSON.dependencies ?? {}),
+    ...Object.keys(PACKAGE_JSON.peerDependencies ?? {})
+]
 
 export default [
     // bundle code
@@ -16,7 +23,7 @@ export default [
             commonjs(),
             nodeResolve(),
         ],
-        external: [/node_modules/],
+        external: external,
         output: [
             {
                 dir: `./dist`,
